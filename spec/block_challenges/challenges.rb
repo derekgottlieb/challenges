e8039f707597f178ae54229f4ae2261c884e9020
Josh Cheek <josh.cheek@gmail.com>
2016-03-04 11:54:01 -0600

    Clean up comments at the bottom of block challenges file

--- spec/block_challenges/challenges.rb
+++ spec/block_challenges/challenges.rb
@@ -46,33 +46,3 @@ class ArrayWithOnlyEach
     self
   end
 end
-
-__END__
-'block_and_forth_forever receives a block, and calls it with a block that doubles whatever it receives'
-results = []
-block_and_forth_forever do |&block|
-  results << block.call(3)
-  results << block.call(2)
-  results << block.call(1)
-end
-assert_equal [3, 2, 1], results
-
-
-'blinked_list_unshift returns a block where, if I call it with :data, it gives me the data, and if I call it with :link, it gives me the original block'
-  'blinked_list_data returns the data, if there is a node'
-  'blinked_list_link returns the link, if there is a node'
-  list = blinked_list_unshift 1, nil
-  list = blinked_list_unshift 'a', list
-  list = blinked_list_unshift :lol, list
-  list.call(:data) # => :lol
-  list.call(:link).call(:datta) # => "a"
-  list.call(:link).call(:link).call(:datta) # => 1
-  list.call(:link).call(:link).call(:link) # => nil
-
-  blinked_list_data list # => :lol
-  list = blinked_list_link list
-  blinked_list_data list # => "a"
-  list = blinked_list_link list
-  blinked_list_data list # => 1
-  list = blinked_list_link list
-  assert_equal nil, list
