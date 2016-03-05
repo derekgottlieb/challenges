ce71188d97199911d747611125dbf84006274fee
Josh Cheek <josh.cheek@gmail.com>
2016-03-05 10:47:11 -0600

    Add link to the material

--- lib/module_challenges.rb
+++ lib/module_challenges.rb
@@ -1,2 +1,3 @@
 # Here's some stuff from when we talked about modules:
-# https://vimeo.com/157849877
+#   https://vimeo.com/157849877
+#   https://github.com/CodePlatoon/curriculum/blob/c459e95c816da69ea2a9807c0fdd57ab18262830/phase1/iterable_project.md
